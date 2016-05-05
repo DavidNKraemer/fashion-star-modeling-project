0ec67f4d1d924ddd6ecbd34741982d992e0ca19d
William <wrebelsky@gmail.com>
2016-05-05 10:55:00 -0500

    Problem_01 added

--- src/problem_01.m
+++ src/problem_01.m
@@ -21,11 +21,13 @@
 % C:   Cotton
 
 % Sunk costs
-sunkcosts = 860000+3*1200000;
+velvet_cost = 500000;
+%velvet_cost = 0;
+sunkcosts = 860000 + 3 * 1200000-velvet_cost;
 
 % Revenue weights
 %                   TWS CAS  SB  SC  TS  WB  VP  CS CM  VS  BB
-revenue_weights  = [300 450 180 120 270 320 350 130 75 200 120];
+revenue_weights  = [300 450 180 120 270 320  0  130 75   0 120];
 labor_weights    = [160 150  80  60 120 140 175  60 40 160  90];
 material_weights = [9 * 3 + 1.5 * 2      ...
                     60 * 1.5             ...
@@ -54,7 +56,7 @@ A = [ 0,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0; %5500
      -1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0; %-(7000*0.6)
       1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0; %7000
       3,   0,   0,   0,   0, 2.5,   0,   0,   0,   0,   0; %45000
-      2,   0,   0,   0, 1.5, 1.5,   0,   0,   2,   0,   0; %28000
+      2,   0,   0,   0, 1.5, 1.5,   2,   0,   0,   0,   0; %28000
       0, 1.5,   0,   0,   0,   0,   0,   0,   0,   0,   0; %9000
       0,   0, 1.5, 0.5,   0,   0,   0,   0,   0,   0,   0; %18000
       0,   0,   0,   0,   2,   0,   0,   0,   0,   0, 1.5; %30000
@@ -64,7 +66,8 @@ A = [ 0,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0; %5500
       0,   0,   0,   0,   0,   0,   0,   1,  -1,   0,   0; %0
  ];
    
-constraints = [5500, 6000,  4000, 12000, 15000, -2800, -3000,  5000, -4200, 7000, 45000, 28000,  9000, 18000, 30000, 20000, 30000,0,0];
+constraints = [5500, 6000,  4000, 12000, 15000, -2800, -3000,  5000, -4200, ...
+               7000, 45000, 28000,  9000, 18000, 30000, 20000, 30000, 0, 0];
 
 lower_bounds = zeros(1,length(net_profit_weights));
 %upper_bounds = 100000 * ones(1, length(net_profit_weights));
@@ -74,4 +77,7 @@ bundle  = linprog(-net_profit_weights, A, constraints, ...
                   [], [], lower_bounds, upper_bounds, []);
 
 result = [constraints; (A * bundle).'];
-Profit = dot(net_profit_weights, bundle) - sunkcosts;
+
+assert(all(result(1,:) >= result(2,:)))
+
+profit = dot(net_profit_weights, bundle) - sunkcosts;
